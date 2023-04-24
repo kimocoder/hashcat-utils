5383c24d481c38db66a402899f37f8721079431f
Sourcery AI <>
2023-04-24 01:37:26 +0000

    'Refactored by Sourcery'

--- src/export_potfile.py
+++ src/export_potfile.py
@@ -8,7 +8,7 @@ for line in sys.stdin:
   if ':' in line:
     passw = line.split(':')[1].rstrip()
     if passw.startswith('$HEX['):
-      hexpassw = re.search('\$HEX\[([a-zA-Z0-9]+)\]', passw).group(1)
+      hexpassw = re.search('\$HEX\[([a-zA-Z0-9]+)\]', passw)[1]
       passw = bytearray.fromhex(hexpassw).decode('latin-1')
     rep = repr(passw)
     if rep.startswith('u'):
